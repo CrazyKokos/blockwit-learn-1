3096a3295fd270bbe2dc1eb9a07a5f708eaa16bf
Admin-Konstantin <beloglazov_k.s@mail.ru>
2021-12-25 23:33:01 +0300

    Initial commit

---
+++ src/main/java/com/blockwit/leran1/crazykokos/Task22.java
@@ -0,0 +1,10 @@
+package com.blockwit.leran1.crazykokos;
+
+public class Task22 {
+    public static void main(String[] args) {
+        int[] a = {23, 56, 21, 67, 98};
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
