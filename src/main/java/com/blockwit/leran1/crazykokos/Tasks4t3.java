92ca7e042f3b1848f12e6e1ab6ac9fdff216ef31
Admin-Konstantin <beloglazov_k.s@mail.ru>
2022-01-11 00:45:05 +0300

    Task4 ready

--- src/main/java/com/blockwit/leran1/crazykokos/Tasks4t3.java
+++ src/main/java/com/blockwit/leran1/crazykokos/Tasks4t3.java
@@ -7,31 +7,21 @@ import java.util.Scanner;
 
 public class Tasks4t3 {
     public static void main(String[] args) {
-        System.out.println("Ваше имя? " + " Ваш год рождения?");
         Scanner in = new Scanner(System.in);
-        Scanner in2 = new Scanner(System.in);
-        String name = in2.nextLine();
-        System.out.println(name);
-        int birtYear = in.nextInt();
-        System.out.println(birtYear);
-        int years = 2022 - birtYear;
-        String name2 = in2.nextLine();
-        System.out.println(name2);
-        int birtYear2 = in.nextInt();
-        System.out.println(birtYear2);
-        int years2 = 2022 - birtYear2;
-        String name3 = in2.nextLine();
-        System.out.println(name3);
-        int birtYear3 = in.nextInt();
-        System.out.println(birtYear3);
-        int years3 = 2022 - birtYear3;
-        String name4 = in2.nextLine();
-        System.out.println(name4);
-        int birtYear4 = in.nextInt();
-        System.out.println(birtYear);
-        int years4 = 2022 - birtYear4;
-        System.out.println(name + " вам " + years + " лет " + name2 + " вам " + years + " лет" + name3 + " вам " + years + " лет " + name4 + " вам " + years + " лет ");
+        int count = 4;
+        String[] names = new String[count];
+        int[] birthdays = new int[count];
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(" Введите имя: ");
+            names[i] = in.nextLine();
+            System.out.println(" Введите год рождения: ");
+            birthdays[i] = in.nextInt();
+            in.nextLine();
+        }
+        for (int i = 0; i < names.length; i++)
+            System.out.println(names[i] + " , Вам лет " + (2022 - birthdays[i]));
+
         in.close();
     }
-
 }
+
