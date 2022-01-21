8f9a0f94f6adb9b88e1f682afd59ebe1c20eefab
Admin-Konstantin <beloglazov_k.s@mail.ru>
2022-01-21 22:25:25 +0300

    class Book completed

---
+++ src/main/java/com/blockwit/leran1/crazykokos/Book.java
@@ -0,0 +1,10 @@
+/*Написать класс книги. У книги должно быть название, автор и год выпуска.*/
+
+package com.blockwit.leran1.crazykokos;
+
+public class Book {
+    String name;
+    String author;
+    int yearOfRelease;
+}
+
