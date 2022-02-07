615f5e6947e49be15c496bf183ac77c7a9b7083f
Admin-Konstantin <beloglazov_k.s@mail.ru>
2022-02-07 22:20:59 +0300

    TaskS74 new completed

--- src/main/java/lesson6/TaskS74.java
+++ src/main/java/lesson6/TaskS74.java
@@ -8,10 +8,12 @@ package lesson6;
 
 public class TaskS74 {
     public static void main(String[] args) {
-        ProtoList1 array1 = new ProtoList1();
+        ProtoList1 array1 = new ProtoList1();       //создание  экземпляра класса
         ProtoList1 array2 = new ProtoList1();
-        int[] a1 = array1.create(7);
-        int[] a2 = array2.create(3);
+        array1.create(7);
+        array2.create(3);                       //вызвал метод и передал размер массива
+        int[] a1 = array1.array;                 //создали массив int и присвоили ему экземпляр поля array
+        int[] a2 = array2.array;
         System.out.println(array1.getSize(a1));
         System.out.println(array2.getSize(a2));
         System.out.println(array1.getIndex(a1, 4));
