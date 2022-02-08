1151108cbaa7ed89a4184445b2282652e1d87c13
Admin-Konstantin <beloglazov_k.s@mail.ru>
2022-02-08 21:34:25 +0300

    ProtoList1 new completed

--- src/main/java/lesson6/ProtoList1.java
+++ src/main/java/lesson6/ProtoList1.java
@@ -7,18 +7,21 @@
 package lesson6;
 
 public class ProtoList1 {
-    int[] array;
+    int[] array;            //поле под названием array типа массив целых чисел
 
-    public int[] create(int size) {
-        int[] array = new int[size];
-        return array;
+    public void create(int size) {
+        int[] array = new int[size];            // метод create который принимает размер и создает и присваивает// полю array пустой массив целых чисел указанного размера
+        this.array = array;
+
     }
 
-    public int getSize(int[] array) {
+
+    public int getSize(int[] array) {           //метод getSize который возращает длину массива в поле array
+
         return array.length;
     }
 
-    public int getIndex(int[] array, int index) {
+    public int getIndex(int[] array, int index) {      // метод get, который принимает индекс и возращает элемент из массива array по указанному индексу
         return array[index];
     }
 }
