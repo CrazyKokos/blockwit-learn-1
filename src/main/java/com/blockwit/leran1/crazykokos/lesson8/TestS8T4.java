a4cf11e04929e5d3705d6c10b1ac48fadd0e582a
Admin-Konstantin <beloglazov_k.s@mail.ru>
2022-02-14 22:38:43 +0300

    TestS8T4

---
+++ src/main/java/com/blockwit/leran1/crazykokos/lesson8/TestS8T4.java
@@ -0,0 +1,10 @@
+package com.blockwit.leran1.crazykokos.lesson8;
+
+public class TestS8T4 {
+
+    public static void main(String[] args) {
+        MyIntArrayList myIntArrayList = new MyIntArrayList();
+        ListsEvaluation listsEvaluation = new ListsEvaluation();
+        listsEvaluation.evalMyList(myIntArrayList);
+    }
+}
